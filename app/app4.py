4c64ffe60a9377ee3fc973d3e8d2c39afc7a600b
ats43 <85419691+ats43@users.noreply.github.com>
2021-08-02 20:41:39 -0400

    task: routes with variables

--- app/app4.py
+++ app/app4.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/4")
-@app.route("/index4")
 @app.route("/home4")
 def home4():
     return "Hello World"
+
+
+@app.route("/user/<username>", methods=["GET", "POST", "PUT"])
+def profile(username):
+    return "This is the username page"
+
+
+@app.route("/index4")
+def index4():
+    return render_template("index4.html",
+                           title="INDEX PAGE",
+                           body="This is the body")
 
 
 if __name__ == '__main__':
