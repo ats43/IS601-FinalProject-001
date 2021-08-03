b96d87efbc7a7cacb6e7f723a2b63d20ce4c9114
ats43 <85419691+ats43@users.noreply.github.com>
2021-08-02 22:05:56 -0400

    task: secret key

--- app/app5.py
+++ app/app5.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 
 # Flask app creation
 app = Flask(__name__)
+# app.config.from_pyfile('config.py')
+app.config.from_object('config.Config')
 
 # Ugly and confusing tangent of in-line config stuff
 app.config['TESTING'] = True
@@ -16,3 +18,7 @@ app.config['DEBUG'] = False  # actually I want debug to be off now
 @app.route('/5')
 def home():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
